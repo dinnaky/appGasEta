38cb0b58a6d740cd40272f416e1a77d7a1df4e5d
Arthur <arhturvitorsilvio@gmail.com>
2024-05-20 22:20:04 -0300

    IMPLEMENTATION OF THE BUTTONS CALC, CLEAR AND FINISH

--- app/src/main/java/devandroid/arthursilvio/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/arthursilvio/appgaseta/view/GasEtaActivity.java
@@ -1,8 +1,10 @@
 package devandroid.arthursilvio.appgaseta.view;
-
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +12,75 @@ import devandroid.arthursilvio.appgaseta.R;
 import devandroid.arthursilvio.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
+    EditText editEta;
+    EditText editGas;
+    Button buttonCalc;
+    TextView txtResult;
+    Button buttonClean;
+    Button buttonSave;
+    Button buttonFinish;
+
+    double mGas;
+    double mEta;
+    String recommendation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseta);
 
-        Toast.makeText(GasEtaActivity.this,
-                UtilGasEta.calculateBestOption(5.12, 3.90),
-                Toast.LENGTH_LONG).show();
+        editEta = findViewById(R.id.editEta);
+        editGas = findViewById(R.id.editGas);
+        buttonCalc = findViewById(R.id.buttonCalc);
+        txtResult = findViewById(R.id.txtResult);
+        buttonClean = findViewById(R.id.buttonClean);
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonFinish = findViewById(R.id.buttonFinish);
+
+        buttonCalc.setOnClickListener(v -> {
+            boolean isDataOk = true;
+
+            if (TextUtils.isEmpty(editEta.getText())){
+                editEta.setError("* OBRIGATÓRIO");
+                editEta.requestFocus();
+                isDataOk = false;
+            }
+
+            if (TextUtils.isEmpty(editGas.getText())){
+                editGas.setError("* OBRIGATÓRIO");
+                editGas.requestFocus();
+                isDataOk = false;
+            }
+
+            if (isDataOk){
+
+                mEta = Double.parseDouble(editEta.getText().toString());
+                mGas = Double.parseDouble(editGas.getText().toString());
+
+                recommendation = UtilGasEta.calculateBestOption(mEta, mGas);
+
+                txtResult.setText(recommendation);
+            }else{
+                Toast.makeText(GasEtaActivity.this, "Digite os dados corretamente...",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        buttonClean.setOnClickListener(v -> {
+            editGas.setText("");
+            editEta.setText("");
+        });
+
+        buttonFinish.setOnClickListener(v -> {
+            Toast.makeText(GasEtaActivity.this, "Finalizado",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        });
+
+        buttonSave.setOnClickListener(v -> {
+
+        });
+
     }
 }

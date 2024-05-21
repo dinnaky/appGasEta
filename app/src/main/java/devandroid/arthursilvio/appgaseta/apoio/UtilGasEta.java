ac0fe1ce1e997f882e53ba0140bdec4056fae0a8
Arthur <arhturvitorsilvio@gmail.com>
2024-05-20 21:50:30 -0300

    IMPLEMENTATION OF THE CALCULATION

---
+++ app/src/main/java/devandroid/arthursilvio/appgaseta/apoio/UtilGasEta.java
@@ -0,0 +1,17 @@
+package devandroid.arthursilvio.appgaseta.apoio;
+
+public class UtilGasEta {
+    public static String calculateBestOption (double gas, double etanol){
+
+        double bestPrice = gas * 0.70;
+        String returnMessage;
+
+        if (etanol<=bestPrice) {
+            returnMessage = "Abastecer com Etanol";
+        }else{
+            returnMessage = "Abastecer com Gasolina";
+        }
+
+        return returnMessage;
+    }
+}
--- app/src/main/java/devandroid/arthursilvio/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/arthursilvio/appgaseta/view/GasEtaActivity.java
@@ -1,18 +1,23 @@
 package devandroid.arthursilvio.appgaseta.view;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.arthursilvio.appgaseta.R;
+import devandroid.arthursilvio.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_gaseta);
+
+        Toast.makeText(GasEtaActivity.this,
+                UtilGasEta.calculateBestOption(5.12, 3.90),
+                Toast.LENGTH_LONG).show();
     }
 }
